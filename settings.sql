c976ef3e000893e4f3de94107370d0fb6afaedaa
Alejandra Valdivia <alexvcodes@gmail.com>
2024-08-04 19:24:23 -0700

    feat: add tunr_django app

---
+++ settings.sql
@@ -0,0 +1,9 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tunr',
+        'USER': 'tunruser',
+        'PASSWORD': 'tunr',
+        'HOST': 'localhost'
+    }
+}
---
+++ test_import.py
@@ -0,0 +1,5 @@
+try:
+    import django_extensions
+    print("django_extensions is installed and can be imported.")
+except ImportError:
+    print("django_extensions is not installed.")
